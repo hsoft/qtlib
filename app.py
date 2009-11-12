4d3f44d89c07553553baebae27053501be0ccbd5
hsoft <none@none>
2009-11-12 14:41:43 +0000

    qtlib: fixed some license mess up.
    --HG--
    extra : convert_revision : svn%3A7b8128f5-b82e-4125-a33a-c49d6b68abe0/qtlib/trunk%4075

--- app.py
+++ app.py
@@ -4,9 +4,9 @@
 # $Id$
 # Copyright 2009 Hardcoded Software (http://www.hardcoded.net)
 # 
-# This software is licensed under the "HS" License as described in the "LICENSE" file, 
+# This software is licensed under the "BSD" License as described in the "LICENSE" file, 
 # which should be included with this package. The terms are also available at 
-# http://www.hardcoded.net/licenses/hs_license
+# http://www.hardcoded.net/licenses/bsd_license
 
 from PyQt4.QtCore import SIGNAL, QTimer, QObject
 
