0ebc7c636648cb6feeeeff14bd790b7ab39bf46b
Virgil Dupras <hsoft@hardcoded.net>
2011-02-18 16:02:12 +0100

    Added italian translation for Fairware pop up.

--- lang/it.ts
+++ lang/it.ts
@@ -1,6 +1,98 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="it_IT">
+<!-- ******** RegDemoDialog ******** -->
+<context>
+    <name>RegDemoDialog</name>
+    <message>
+        <source>$appname is Fairware</source>
+        <translation>$appname è Fairware</translation>
+    </message>
+    <message>
+        <source>Please contribute</source>
+        <translation>Contribuisci</translation>
+    </message>
+    <message>
+        <source>Unpaid hours: $unpaid</source>
+        <translation>Ore non pagate: $unpaid</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Continua</translation>
+    </message>
+    <message>
+        <source>Enter Key</source>
+        <translation>Inserisci Codice</translation>
+    </message>
+    <message>
+        <source>Contribute</source>
+        <translation>Contribuisci</translation>
+    </message>
+    <message>
+        <source>More Info</source>
+        <translation>Altre Informazioni</translation>
+    </message>
+    <message>
+        <source>I will not contribute, stop reminding me</source>
+        <translation>Non contribuisco, non ricordarmelo più</translation>
+    </message>
+</context>
+
+<!-- ******** RegDontContributeDialog ******** -->
+<context>
+    <name>RegDontContributeDialog</name>
+    <message>
+        <source>Don't contribute</source>
+        <translation>Non contribuisco</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Invia</translation>
+    </message>
+    <message>
+        <source>You indicated not wanting to (or not being able to) contribute.</source>
+        <translation>Hai scelto di non voler (o non poter) contribuire.</translation>
+    </message>
+</context>
+
+<!-- ******** RegSubmitDialog ******** -->
+<context>
+    <name>RegSubmitDialog</name>
+    <message>
+        <source>Contribute</source>
+        <translation>Contribuisci</translation>
+    </message>
+    <message>
+        <source>Enter your registration key</source>
+        <translation>Inserisci il tuo codice</translation>
+    </message>
+    <message>
+        <source>Registration key:</source>
+        <translation>Codice di registrazione:</translation>
+    </message>
+    <message>
+        <source>Registered e-mail:</source>
+        <translation>E-mail di riferimento:</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>Submit</source>
+        <translation>Invia</translation>
+    </message>
+    <message>
+        <source>Registration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This code is valid. Thanks!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+
+<!-- ******** Message ******** -->
 <context>
 <name>message</name>
 <message>
