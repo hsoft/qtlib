3207aa547f7e1e42b7d4ac607347c6d0cb1412a2
Virgil Dupras <hsoft@hardcoded.net>
2011-07-13 10:20:17 -0400

    Added german localization (by Gregor Taetzner).

--- lang/de.ts
+++ lang/de.ts
@@ -1,21 +1,100 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="de_DE">
+<!-- ******** RegDemoDialog ******** -->
+<context>
+    <name>RegDemoDialog</name>
+    <message>
+        <source>$appname is Fairware</source>
+        <translation>$appname ist Fairware</translation>
+    </message>
+    <message>
+        <source>Please contribute</source>
+        <translation>Bitte spenden</translation>
+    </message>
+    <message>
+        <source>Unpaid hours: $unpaid</source>
+        <translation>Unbezahlte Stunden: $unpaid</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Fortfahren</translation>
+    </message>
+    <message>
+        <source>Enter Key</source>
+        <translation>Schlüssel eingeben</translation>
+    </message>
+    <message>
+        <source>Contribute</source>
+        <translation>Spenden</translation>
+    </message>
+    <message>
+        <source>More Info</source>
+        <translation>Mehr Infos</translation>
+    </message>
+</context>
+
+<!-- ******** RegSubmitDialog ******** -->
+<context>
+    <name>RegSubmitDialog</name>
+    <message>
+        <source>Contribute</source>
+        <translation>Spenden</translation>
+    </message>
+    <message>
+        <source>Enter your registration key</source>
+        <translation>Geben Sie ihren Schlüssel ein</translation>
+    </message>
+    <message>
+        <source>Registration key:</source>
+        <translation>Registrierungsschlüssel:</translation>
+    </message>
+    <message>
+        <source>Registered e-mail:</source>
+        <translation>Registrierte E-Mail:</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+    <message>
+        <source>Submit</source>
+        <translation>Abschicken</translation>
+    </message>
+    <message>
+        <source>Registration</source>
+        <translation>Registrierung</translation>
+    </message>
+    <message>
+        <source>This code is valid. Thanks!</source>
+        <translation>Der Schlüssel ist valide, Danke!</translation>
+    </message>
+    <message>
+        <source>Tell Hardcoded Software which operating system I'm using.</source>
+        <translation>Übermittle Hardcoded Software welches Betriebssystem ich benutze.</translation>
+    </message>
+    <message>
+        <source>(to have some contribution statistics based on OSes)</source>
+        <translation>(für Spendenstatistiken, basierend auf den Betriebssystemen)</translation>
+    </message>
+</context>
+
+<!-- ******** Message ******** -->
 <context>
 <name>message</name>
 <message>
     <source>FairwarePromptMsg</source>
-    <translation>$appname is Fairware, which means &quot;open source software developed with expectation of fair contributions from users&quot;. Hours have been invested in this software with the expectation that users will be fair enough to compensate them. The &quot;Unpaid hours&quot; figure you see below is the hours that have yet to be compensated for this project.
-    
-If you like this application, please make a contribution that you consider fair. Thanks!
+    <translation>$appname ist Fairware, das bedeutet &quot;Open Source Software, entwickelt in der Hoffnung auf einen fairen Beitrag von den Benutzern&quot;. Stunden wurden in die Software investiert, mit der Erwartung der Nutzer möge fair genug sein die Entwickler zu kompensieren. Die &quot;Unbezahlte Stunden&quot; Abbildung zeigt die Anzahl der Stunden die noch nicht finanziert wurden.
 
-If you cannot afford to contribute, you can either ignore this reminder or send an e-mail at support@hardcoded.net so I can give you a registration key.
+Wenn Sie diese Anwendung mögen, so spenden Sie bitte einen Ihrer Ansicht nach angemessenen Betrag. Danke!
 
-This dialog doesn't show when there are no unpaid hours or when you have a valid contribution key.</translation>
+Wenn Sie es sich nicht leisten können zu spenden, können Sie diese Erinnerung entweder ignorieren oder mir eine Nachricht an hsoft@hardcoded.net schicken für einen Registrierungsschlüssel.
+
+Dieser Dialog erscheint nicht, wenn es keine unbezahlten Stunden gibt oder Sie einen gülten Registrierungsschlüssel besitzen.</translation>
 </message>
 <message>
     <source>FairwareTypeKeyMsg</source>
-    <translation>Type the key you received when you contributed to $appname, as well as the e-mail used as a reference for the purchase.</translation>
+    <translation>Geben sie den empfangenen Schlüssel und die E-Mail-Adresse als Referenz für den Kauf an, wenn sie für $appname gespendet haben.</translation>
 </message>
 </context>
 </TS>
