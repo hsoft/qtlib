65b8d9fa9218b9286a653effe891a6ac227f47f1
Virgil Dupras <hsoft@hardcoded.net>
2011-09-03 10:17:32 -0400

    Added lang/zh.ts (incomplete for now).

---
+++ lang/zh.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+<name>message</name>
+<message>
+    <source>FairwarePromptMsg</source>
+    <translation>$appname 是一款捐助软件,也就是说 &quot;用户对研发开源软件所花费的时间进行符合用户意愿的捐助&quot;。用户可以根据研发人员花费在开发软件上的时间进行合理的补偿。用户在下面看到的 &quot;未支付的时间&quot; (Unpaid hours)表示需要对该软件进行补偿的时间。
+    
+如果您喜欢这款软件，我诚挚的希望您可以进行必要的捐助。谢谢！
+
+如果您无法承担捐助费用，您也可以忽略此提醒，或者发送电子邮件至 support@hardcoded.net ，我会发送给您一个注册密钥。
+
+当软件没有未支付的时间或您已使用一个有效的注册密钥，此对话框将不会再显示。</translation>
+</message>
+<message>
+    <source>FairwareTypeKeyMsg</source>
+    <translation>当您捐助 $appname 后，请输入收到的注册密钥以及电子邮件,这将作为购买凭证。</translation>
+</message>
+</context>
+</TS>
