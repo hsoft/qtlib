f15c5800ca7b714b5cbc554405032f473a6d8455
Virgil Dupras <hsoft@hardcoded.net>
2011-09-03 13:27:57 -0400

    Completed chinese localization.

--- lang/zh.ts
+++ lang/zh.ts
@@ -1,6 +1,85 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="zh_CN">
+<!-- ******** RegDemoDialog ******** -->
+<context>
+    <name>RegDemoDialog</name>
+    <message>
+        <source>$appname is Fairware</source>
+        <translation>$appname 是一款捐助型软件</translation>
+    </message>
+    <message>
+        <source>Please contribute</source>
+        <translation>期待您的捐助</translation>
+    </message>
+    <message>
+        <source>Unpaid hours: $unpaid</source>
+        <translation>未支付的时间: $unpaid</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>继续</translation>
+    </message>
+    <message>
+        <source>Enter Key</source>
+        <translation>输入密钥</translation>
+    </message>
+    <message>
+        <source>Contribute</source>
+        <translation>捐助</translation>
+    </message>
+    <message>
+        <source>More Info</source>
+        <translation>更多信息</translation>
+    </message>
+</context>
+
+<!-- ******** RegSubmitDialog ******** -->
+<context>
+    <name>RegSubmitDialog</name>
+    <message>
+        <source>Contribute</source>
+        <translation>捐助</translation>
+    </message>
+    <message>
+        <source>Enter your registration key</source>
+        <translation>输入密钥</translation>
+    </message>
+    <message>
+        <source>Registration key:</source>
+        <translation>密钥:</translation>
+    </message>
+    <message>
+        <source>Registered e-mail:</source>
+        <translation>注册的电子邮箱:</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+    <message>
+        <source>Submit</source>
+        <translation>提交</translation>
+    </message>
+    <message>
+        <source>Registration</source>
+        <translation>注册</translation>
+    </message>
+    <message>
+        <source>This code is valid. Thanks!</source>
+        <translation>此密钥有效，感谢您的注册!</translation>
+    </message>
+    <message>
+        <source>Tell Hardcoded Software which operating system I'm using.</source>
+        <translation>告诉Hardcoded Software您正在使用什么操作系统。</translation>
+    </message>
+    <message>
+        <source>(to have some contribution statistics based on OSes)</source>
+        <translation>(这将有助于根据操作系统来进行捐助统计)</translation>
+    </message>
+</context>
+
+<!-- ******** Message ******** -->
 <context>
 <name>message</name>
 <message>
@@ -9,7 +88,7 @@
     
 如果您喜欢这款软件，我诚挚的希望您可以进行必要的捐助。谢谢！
 
-如果您无法承担捐助费用，您也可以忽略此提醒，或者发送电子邮件至 support@hardcoded.net ，我会发送给您一个注册密钥。
+如果您无法承担捐助，您也可以忽略此提醒，或者发送电子邮件至 support@hardcoded.net ，我会发送给您一个注册密钥。
 
 当软件没有未支付的时间或您已使用一个有效的注册密钥，此对话框将不会再显示。</translation>
 </message>
