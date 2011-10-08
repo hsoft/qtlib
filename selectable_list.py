2da810bd849c2f22c7695228d06c2d5ace8bb389
Virgil Dupras <hsoft@hardcoded.net>
2011-10-08 15:18:17 -0400

    Handle update_selection() in ComboboxModel.

--- selectable_list.py
+++ selectable_list.py
@@ -34,22 +34,30 @@ class SelectableList(QAbstractListModel):
     def _updateSelection(self):
         raise NotImplementedError()
     
+    def _restoreSelection(self):
+        raise NotImplementedError()
+    
     #--- model --> view
     def refresh(self):
         self.reset()
-        self._updateSelection()
+        self._restoreSelection()
+    
+    def update_selection(self):
+        self._restoreSelection()
 
 class ComboboxModel(SelectableList):
     def __init__(self, model, view):
         SelectableList.__init__(self, model, view)
         self.view.currentIndexChanged[int].connect(self.selectionChanged)
-        self._updateSelection()
     
     #--- Override
     def _updateSelection(self):
         index = self.view.currentIndex()
         if index != self.model.selected_index:
             self.model.select(index)
+    
+    def _restoreSelection(self):
+        self.view.setCurrentIndex(self.model.selected_index)
     
     #--- Events
     def selectionChanged(self, index):
@@ -67,6 +75,9 @@ class ListviewModel(SelectableList):
         if newIndexes != self.model.selected_indexes:
             self.model.select(newIndexes)
     
+    # XXX Implement _restoreSelection() for dupeGuru (the only user so far of ListviewModel)
+
     #--- Events
     def selectionChanged(self, index):
         self._updateSelection()
+    
