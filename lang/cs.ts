8b34b238e34d1d685abd1848289e0344665adc5c
Virgil Dupras <hsoft@hardcoded.net>
2011-10-10 17:36:59 -0400

    Added Czech language file (placeholder).

---
+++ lang/cs.ts
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<!-- ******** RegDemoDialog ******** -->
+<context>
+    <name>RegDemoDialog</name>
+    <message>
+        <source>$appname is Fairware</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please contribute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unpaid hours: $unpaid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter Key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Contribute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>More Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Try</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Buy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+
+<!-- ******** RegSubmitDialog ******** -->
+<context>
+    <name>RegSubmitDialog</name>
+    <message>
+        <source>Contribute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Enter your registration key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Registration key:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Registered e-mail:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Submit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Registration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This code is valid. Thanks!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Tell Hardcoded Software which operating system I'm using.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>(to have some contribution statistics based on OSes)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+
+<!-- ******** Message ******** -->
+<context>
+<name>message</name>
+<message>
+    <source>FairwarePromptMsg</source>
+    <translation>{name} is Fairware, which means &quot;open source software developed with expectation of fair contributions from users&quot;. Hours have been invested in this software with the expectation that users will be fair enough to compensate them. The &quot;Unpaid hours&quot; figure you see below is the hours that have yet to be compensated for this project.
+    
+If you like this application, please make a contribution that you consider fair. Thanks!
+
+If you cannot afford to contribute, you can either ignore this reminder or send an e-mail at support@hardcoded.net so I can give you a registration key.
+
+This dialog doesn't show when there are no unpaid hours or when you have a valid contribution key.</translation>
+</message>
+<message>
+    <source>DemoPromptMsg</source>
+    <translation>{name} is Fairware, which means &quot;open source software developed with expectation of fair contributions from users&quot;. It's a very interesting concept, but one year of fairware has shown that most people just want to know how much it costs and not be bothered with theories about intellectual property.
+    
+So I won't bother you and will be very straightforward: You can try {name} for free but you have to buy it in order to use it without limitations. In demo mode, {name} {limitation}.
+
+So it's as simple as this. If you're curious about fairware, however, I encourage you to read more about it by clicking on the &quot;Fairware?&quot; button.</translation>
+</message>
+<message>
+    <source>FairwareTypeKeyMsg</source>
+    <translation>Type the key you received when you contributed to $appname, as well as the e-mail used as a reference for the purchase.</translation>
+</message>
+</context>
+</TS>
