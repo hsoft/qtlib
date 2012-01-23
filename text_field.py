3ff87e470e9170beb5529b3d0ce1913c8ff55bec
Virgil Dupras <hsoft@hardcoded.net>
2012-01-23 11:27:17 -0500

    Added text_field unit.

---
+++ text_field.py
@@ -0,0 +1,21 @@
+# Created On: 2012/01/23
+# Copyright 2011 Hardcoded Software (http://www.hardcoded.net)
+# 
+# This software is licensed under the "BSD" License as described in the "LICENSE" file, 
+# which should be included with this package. The terms are also available at 
+# http://www.hardcoded.net/licenses/bsd_license
+
+class TextField:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+        self.model.view = self
+        self.view.editingFinished.connect(self.editingFinished)
+    
+    def editingFinished(self):
+        self.model.text = self.view.text()
+    
+    # model --> view
+    def refresh(self):
+        self.view.setText(self.model.text)
+    
